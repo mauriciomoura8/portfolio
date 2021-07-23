bd07f02484ded2a8ece109e962d1bfbc47a9081b
mauriciomoura8 <mauricio_rockr@hotmail.com>
2021-07-23 01:00:40 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
+
 function setClock(){
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds()/60
